642f94f3635a8ec7b0d7b1157ea5410bc0bd0986
Thiago <thirauj@br.ibm.com>
2021-01-15 16:29:26 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+function Home() {
+    return (
+        <div class="main">
+            <Head>
+                <title>JatoClean</title>
+            </Head>
+            <div class="main-top"> 
+                <div class="logo"></div>
+            </div>
+            <div class="main-bottom"> Meio </div>
+        </div>
+    )
+}
+
+export default Home
