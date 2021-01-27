dfa8f7a470bc332cd3bd75c5a73c8bb643ccf879
Thiago <thirauj@br.ibm.com>
2021-01-27 16:34:56 -0300

    Cria novo estilo de pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from "next/head"
 import Image from "next/image"
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 
 function Home() {
     return (
@@ -13,11 +19,31 @@ function Home() {
                 </div>
             </div>
             <div class="main-bottom">
-                <div class="main-content">
-
+                <div class="main-bottom-title-box">
+                    <div class="main-bottom-title">
+                        <p class="text-big">MANTENHA SEU AMBIENTE LIMPO E PROTEGIDO</p>
+                        <p class="text-mid">LAVAGEM E HIGIENIZAÇÃO DE CARROS E ESTOFADOS</p>
+                    </div>
                 </div>
-            
-            
+                <div class="main-bottom-images-box"></div>
+                <div class="main-bottom-social-box">
+                    <div class="main-bottom-social-text">
+                        <span class="text-mid">
+                            ENTRE EM CONTATO CONOSCO E SOLICITE SEU ORÇAMENTO
+                        </span>
+                    </div>
+                    <div class="main-bottom-social-icons">
+                        <div class="icon-social">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </div>
+                        <div class="icon-social">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </div>
+                        <div class="icon-social">
+                            <FontAwesomeIcon icon={faWhatsapp} />
+                        </div>
+                    </div>
+                </div>
              </div>
         </div>
     )
